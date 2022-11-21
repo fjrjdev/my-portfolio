3242caeccdecec76fbbf128e3e9148af421de11b
fjrjdev <thedromemafia@gmail.com>
2022-11-21 20:04:00 -0300

    initial commit

---
+++ src/utils/stackData.ts
@@ -0,0 +1,42 @@
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiPostgresql, SiTypescript, SiMaterialui } from "react-icons/si";
+import { FaGit, FaPython } from "react-icons/fa";
+import {
+  FaDocker,
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaNode,
+  FaReact,
+} from "react-icons/fa";
+import { DiDjango } from "react-icons/di";
+
+export const stackData = [
+  {
+    title: "HTML",
+    img: FaHtml5,
+  },
+  {
+    title: "CSS",
+    img: FaCss3Alt,
+  },
+  {
+    title: "JS",
+    img: FaJs,
+  },
+  { title: "TypeScript", img: SiTypescript },
+  {
+    title: "Node JS",
+    img: FaNode,
+  },
+  {
+    title: "React",
+    img: FaReact,
+  },
+  { title: "Material UI", img: SiMaterialui },
+  { title: "Docker", img: FaDocker },
+  { title: "PostgreSQL", img: SiPostgresql },
+  { title: "Git", img: FaGit },
+  { title: "Python", img: FaPython },
+  { title: "Django", img: DiDjango },
+];
---
+++ src/utils/userData.ts
@@ -0,0 +1,8 @@
+export const userData = {
+  nameUser: "Fabio Junior",
+  githubUser: "fjrjdev",
+  linkedinUser: "fjdev",
+  whatsappNumber: "+5521995368197",
+  emailUser: "contatofjrj@gmail.com",
+  instagramUser: "fabiodk5",
+};
